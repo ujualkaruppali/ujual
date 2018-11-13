633b7bbd01aa9fffd5d32d448d141b022e92ad73
ubuntu <ujualkaruppali@gmail.com>
2018-11-13 16:15:41 +0530

    tuesday

--- python/flaskpgms/app.py
+++ python/flaskpgms/app.py
@@ -1,18 +1,27 @@
-from flask import Flask,render_template
+
+
+
+
+from flask import Flask,render_template ,request
 app=Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'welcome to flask'
+@app.route('/send',methods=['GET','POST'])
+def send():
+    if(request.method=='POST'):
+        getName=request.form['nm']
+        return render_template('result.html',newName=getName)
+       
+
 @app.route('/about')
-def hij():
-    return 'welcome to about'
+def abt():
+    return render_template('about.html')
 @app.route('/home')
-def hijk():
+def hme():
     return render_template('home.html')
-@app.route('/icta')
-def icta():
-    return 'welcome to icta'
+@app.route('/contact')
+def cnt():
+    return render_template('contact.html')
+
 
 if(__name__=='__main__'):
     app.run(debug=True)
